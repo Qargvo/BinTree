568b5b114388a3fbf228c003f48b80dd3a14904f
V <qargvo@gmail.com>
2019-05-25 20:23:00 +0300

    add built empty tree

--- Project1.dpr
+++ Project1.dpr
@@ -13,13 +13,32 @@ type
   end;
 
   var
-    k,n:Cardinal;
-function level(inp:Cardinal);
+    n:Cardinal;
+    k:Byte;
+procedure get_empty_tree(knot:PntBinTree;inp:byte);
 
+begin
+if inp > 1 then
+  New(knot^.gnext);
+  get_empty_tree(knot^.GNext,inp-1);
+  New(knot^.lnext);
+  get_empty_tree(knot^.Lnext,inp-1);
+end;
+function get_level(inp:cardinal):byte;
+var i:Cardinal;
+begin
+  Result:=1;
+  i:=2;
+  while i < inp do
+    begin
+      Inc(Result);
+      Inc(i,i);
+    end;
+end;
 begin
 Write('Input amount of numbers: ');
 Readln(n);
-
+get_empty_tree();
 
 end.
  
